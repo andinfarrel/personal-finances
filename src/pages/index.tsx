37fbe271dcf37ea9b0f43c206b8de9044cbbd58b
andinfarrel <andinfarrel@gmail.com>
2022-07-10 11:37:00 +0100

    group delete handler

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,18 +15,19 @@ import {
 import { v4 as uuidv4 } from "uuid";
 
 const Home: NextPage = () => {
-  const [groups, setGroups] = useState([{ label: "Finance Sheet 1" }]);
-  const deleteGroup = (groupId: string) => {
+  const [groups, setGroups] = useState([{ groupId: uuidv4(), label: "Finance Sheet 1" }]);
+  const handleDeleteGroup = (groupId: string) => {
+    setGroups(groups.filter(group => group.groupId !== groupId))
   }
 
   return (
     <div className="min-h-screen flex flex-col bg-slate-100">
       <div className="my-auto p-4 sm:p-8 md:p-36 rounded-lg flex flex-col space-y-8">
         {groups.map((group, index) => (
-          <GroupContainer key={index} label={group.label}/>
+          <GroupContainer key={index} label={group.label} id={group.groupId} handleDeleteGroup={handleDeleteGroup}/>
         ))}
         <div className="my-auto mx-auto">
-          <button onClick={() => setGroups([...groups, { label: `Finance Sheet ${groups.length + 1}` }])} className="text-slate-50 px-4 py-2 aspect-square  bg-slate-800 rounded-xl text-4xl">+</button>
+          <button onClick={() => setGroups([...groups, { groupId: uuidv4(), label: `Finance Sheet ${groups.length + 1}` }])} className="text-slate-50 px-4 py-2 aspect-square  bg-slate-800 rounded-xl text-4xl">+</button>
         </div>
       </div>
     </div>
@@ -41,8 +42,10 @@ interface ListItem {
 }
 
 const GroupContainer: FC<{
+  id: string;
   label: string;
-}> = ({ label }) => {
+  handleDeleteGroup: (groupId: string) => void;
+}> = ({ label, handleDeleteGroup, id }) => {
   const [listItems, setListItems] = useState<ListItem[]>([{ rowId: uuidv4(), name: "example", amount: 400, type: "add" }]);
   const listVals = Object.values(listItems);
   const pieData = listVals.map((val) => ({ name: val.name, value: val.amount }));
@@ -78,7 +81,7 @@ const GroupContainer: FC<{
     <div className="mx-auto bg-slate-50 w-full max-w-xl flex flex-col space-y-4 rounded-lg p-4 d:p-8">
       <div className="flex justify-between py-4">
         <p className="text-xl font-semibold italic hover:cursor-pointer">{label}</p>
-        <button className="text-red-500">Delete</button>
+        <button onClick={() => handleDeleteGroup(id)} className="text-red-500">Delete</button>
       </div>
       <form className="flex flex-col space-y-4">
         {listItems &&
